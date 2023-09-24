9275d669801cda3a4976da3e1f803afcab2d7cc3
Tresor-ilunga <19im065@esisalama.org>
2023-09-24 21:12:41 +0200

    Add shadow inputs

---
+++ src/layouts/ShadowList/ShadowColorPicker.jsx
@@ -0,0 +1,5 @@
+export default function ShadowColorPicker() {
+    return (
+        <div>ShadowColorPicker</div>
+    )
+}
---
+++ src/layouts/ShadowList/ShadowRange.jsx
@@ -0,0 +1,5 @@
+export default function ShadowRange() {
+    return (
+        <div>ShadowRange</div>
+    )
+}
