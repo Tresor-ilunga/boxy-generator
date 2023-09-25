0805bea2d0e5252bcfb629a4b92f034654fb417f
Tresor-ilunga <19im065@esisalama.org>
2023-09-25 20:46:47 +0200

    Activate modal

--- src/layouts/Visualisation/Modal/ModalBtn.jsx
+++ src/layouts/Visualisation/Modal/ModalBtn.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+import { createPortal } from "react-dom"
+import ModalResult from "./ModalResult.jsx"
+
 export default function ModalBtn() {
+    const [showModal, setShowModal] = useState(false)
+
     return (
-        <div className="relative z-0 mx-auto mt-2 py-1 cursor-pointer px-3 text-sm rounded bg-blue-600 text-white hover:bg-blue-700">
-            Get the code 
-        </div>
+        <>
+            <button 
+            onClick={() => setShowModal(!showModal)}
+            className="relative z-0 mx-auto mt-2 py-1 cursor-pointer px-3 text-sm rounded bg-blue-600 text-white hover:bg-blue-700">
+                Get the code 
+            </button>
+            {showModal && createPortal(
+                <ModalResult closeModal={() => setShowModal(!showModal)} />,
+                document.body
+            )}
+        </>
     )
 }
